04a9d56945f1f8bc85f792c3dd1ec19182821461
ehalterman <hal17034@byui.edu>
2020-07-03 23:41:14 -0600

    update

--- Week 6/ToDos.js
+++ Week 6/ToDos.js
@@ -3,17 +3,18 @@ document.getElementById("add").onclick = function() {
     var text = document.getElementById("newToDo").value;
     var li = document.createElement("li");
     var ul = document.getElementById("list");
-    li.innerHTML = '<input type="checkbox" id="item' + [id] + '">    ' + text + '   <input type="button" value="X">';
+    li.innerHTML = '<input type="checkbox" id="item' + [id] + '" onclick="checkMyBox(' + [id] +')">    ' + text + '   <input type="button" value="X">';
     id += 1;
     ul.appendChild(li);  
 }
 
-document.getElementsByClassName("check").onclick = function() {
-    var checkBox = document.forms[0];
-    var i;
-    for (i = 0; i < checkBox.length; i++) {
-        if (checkBox[i].checked) {
-            alert("checked" + i );
+function checkMyBox(myId) {
+    checkBox = document.getElementById("item" + myId);
+    var item  = document.querySelector("ul:nth-child("+ parseInt(myId) +")");
+    if (checkBox.checked == true) {
+        alert(item);        }
+    else{
+            alert(myId + " not checked");
         }
     }
-}
+    
